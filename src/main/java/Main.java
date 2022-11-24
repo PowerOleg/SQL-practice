d8023b73c1124de00fdb71ca533da4ff0ed30fb2
PowerOleg <geraskin.oleg@mail.ru>
2022-11-24 21:31:02 +0500

    Main created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
+//        long now = System.currentTimeMillis();
+//        Date sqlDate2 = Date.valueOf(LocalDate.of(1983, Month.JULY, 12));
+//        System.out.println("SqlDate2: " + sqlDate2);
+
+import com.jdbc.pojo.Department;
+import com.jdbc.pojo.Employee;
+
+import java.sql.Date;
+
+
+
 public class Main {
+    public static void main(String[] args) {
+        Date sqlDate1 = Date.valueOf("2001-01-01");
+        Date sqlDate2 = Date.valueOf("2011-01-01");
+        Date sqlDate3 = Date.valueOf("2021-01-01");
+        Date sqlDate4 = Date.valueOf("2022-01-01");
+        Employee employee1 = new Employee(205, sqlDate1, "Danil", 30000);
+        Employee employee2 = new Employee(210, sqlDate2, "Oleg", 40000);
+        Employee employee3 = new Employee(299, sqlDate3, "Aleksandr", 50000);
+        Employee employee4 = new Employee(940, sqlDate4, "Vladimir", 60000);
+
+        Department department1 = new Department();
+        Department department2 = new Department();
+        Department department3 = new Department();
+        Department department4 = new Department();
+    }
 }
--- src/main/java/com/jdbc/pojo/Employee.java
+++ src/main/java/com/jdbc/pojo/Employee.java
@@ -7,14 +7,16 @@ import java.sql.Date;
 public class Employee {
     private int id;
     private int department_id;
+//    long now = System.currentTimeMillis();
+//    Date sqlDate = new Date(now);
     private Date work_start_date;
     private String name;
     private int salary;
 
     public Employee() {}
 
-    public Employee(int id, int department_id, Date work_start_date, String name, int salary) {
-        this.id = id;
+    public Employee(int department_id, Date work_start_date, String name, int salary) {
+//        this.id = id;
         this.department_id = department_id;
         this.work_start_date = work_start_date;
         this.name = name;
